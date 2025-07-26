024bf12f80aaff2b081f88dff715a91dc6bcd884
Abhishek Kumar <github.abhiarya@gmail.com>
2025-07-26 23:44:31 +0530

    learning: better-auth and drizzle

--- apps/web/app/api/auth/[...all]/route.ts
+++ apps/web/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
+import { authServer } from "@current/auth/auth-server";
 import { toNextJsHandler } from "better-auth/next-js";
-import { authServer } from "@current/auth/server";
 
 export const { POST, GET } = toNextJsHandler(authServer);
