f14b6ca7c1235d35c262cc5452c08d2967d084c0
Abhishek Kumar <github.abhiarya@gmail.com>
2025-08-18 01:11:15 +0530

    working on core ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ThemeToggleButton } from "@current/ui/components/theme-switch-button";
 import SignInCornerDialog from "@/components/auth/sign-in-corner-dialog";
 import { SignInDialog } from "@/components/auth/sign-in-dialog";
+import ChatBotDemo from "@/components/chat";
 
 export default function Home() {
   return (
@@ -8,6 +9,8 @@ export default function Home() {
       <ThemeToggleButton />
       <SignInDialog />
       <SignInCornerDialog />
+
+      <ChatBotDemo />
     </div>
   );
 }
